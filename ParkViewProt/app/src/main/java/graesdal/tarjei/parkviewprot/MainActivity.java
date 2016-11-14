a42168d50f12f39637cf0b758fe53dc1e5dc46f9
amtize <tgraes@hotmail.com>
2016-11-14 21:16:38 +0100

    Updated Map from fragment to fragmentActivity.

--- ParkViewProt/app/src/main/java/graesdal/tarjei/parkviewprot/MainActivity.java
+++ ParkViewProt/app/src/main/java/graesdal/tarjei/parkviewprot/MainActivity.java
@@ -1,5 +1,7 @@
 package graesdal.tarjei.parkviewprot;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "LETS GET SOME PUSSY TONIGHT", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(null, MapsActivity.class);
             }
         });
     }
@@ -49,4 +52,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
